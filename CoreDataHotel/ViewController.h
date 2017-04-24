fedc0a252c63a230df1645ac1f3305cf4e0d3a4a
Regenal Grant <regenal@mac.com>
2017-04-24 09:29:36 -0700

    Initial Commit

---
+++ CoreDataHotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataHotel
+//
+//  Created by Regenal Grant on 4/24/17.
+//  Copyright © 2017 regenal grant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
